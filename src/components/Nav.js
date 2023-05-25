b0b750fc6f4895c2c5f7124120f820e522ab7687
monsur776e <monsuralam8897@gmail.com>
2023-05-26 00:40:55 +0600

    start up setup done

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,25 @@
-import React from 'react';
-
+import React from "react";
+import { BiHomeAlt, BiUser } from "react-icons/bi";
+import {
+   BsBriefcase,
+   BsChatSquare,
+   BsClipboard2Data,
+   BsFillChatSquareDotsFill,
+   IconName,
+} from "react-icons/bs";
+import { Link } from "react-scroll";
 const Nav = () => {
-  return <div>Nav</div>;
+   return (
+      <nav className="fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50">
+         <div className="container mx-auto">
+            <div>
+               <Link>
+                  <BiHomeAlt></BiHomeAlt>
+               </Link>
+            </div>
+         </div>
+      </nav>
+   );
 };
 
 export default Nav;
